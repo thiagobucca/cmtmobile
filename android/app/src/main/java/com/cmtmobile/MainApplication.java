55cb6fe3502059add6179d2702954d5c16f0fdba
thiagobucca <dishins@gmail.com>
2019-06-11 15:35:38 -0300

    removido pasta android do gitigonre

---
+++ android/app/src/main/java/com/cmtmobile/MainApplication.java
@@ -0,0 +1,58 @@
+package com.cmtmobile;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.imagepicker.ImagePickerPackage;
+import fr.snapp.imagebase64.RNImgToBase64Package;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends NavigationApplication {
+  @Override
+  protected ReactGateway createReactGateway() {
+    ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+      @Override
+      protected String getJSMainModuleName() {
+        return "index";
+      }
+    };
+    return new ReactGateway(this, isDebug(), host);
+  }
+
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
+
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+            new RNI18nPackage(),
+            new PickerPackage(),
+            new ImagePickerPackage(),
+            new RNImgToBase64Package(),
+            new ReactNativeOneSignalPackage(),
+            new VectorIconsPackage()
+    );
+  }
+}
